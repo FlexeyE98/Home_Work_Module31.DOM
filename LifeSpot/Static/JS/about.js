3129489ba2af0dbd0170fbfe77d7066986822663
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-10-29 17:52:31 +0300

    Разработка слайдера на JS

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,87 @@
+﻿/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+* 
+* */
+
+function Comment() {
+    // Запросим имя
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
+        return
+    }
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
+        return
+    }
+    this.date = new Date().toLocaleString()
+}
+
+    // Сохраним текущее время
+    
+function addComment() {
+    let comment = new Comment()
+
+    if (comment.empty) {
+        return;
+    }
+
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        let review = Object.create(comment)
+        review.rate = 0;
+
+        writeReview(review)
+
+        }
+     else {
+
+        writeReview(comment)
+    }
+}
+
+const writeReview = review => {
+    let likeCounter = '';
+
+    // Если публикуется отзыв - добавляем ему кнопку с лайками.
+    if (review.hasOwnProperty('rate')) {
+
+        let commentId = Math.random();
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+    // Запишем результат 
+    document.getElementsByClassName('reviews')[0].innerHTML += ' <div class="review-    text">\n' + `<p> <i> <b>${review['author']}</b> ${review['date']}${likeCounter}</i></p>` + `<p>${review['text']}</p>` + '</div>';
+}
+
+
+function addLike(id) {
+    // Найдём нужный элемент по id
+    let element = document.getElementById(id);
+
+    // Преобразуем текст элемента в массив, разбив его по пробелам (так как счётчик лайков у нас отделен от символа ❤️пробелом)
+    let array = element.innerText.split(' ')
+
+    // Вытащим искомое значение счётчика и сразу же преобразуем его в число, так как
+    // при сложении любого значения со строкой в JS будет строка, а нам этого не требуется
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    // Увеличим счётчик
+    resultNum += 1
+
+    // Сохраним измененное значение обратно в массив
+    array[array.length - 1] = `${resultNum}`
+
+    // Обновим текст элемента
+    element.innerText = array.join(' ')
+}
+
+
+
+
+
+
+
+
